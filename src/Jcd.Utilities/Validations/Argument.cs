2c9ab9ee18afd6ebde49f45b8a0057a17f919d87
jason-c-daniels <jason.c.daniels@gmail.com>
2018-05-29 22:36:45 -0500

    Added Unit Tests for Argument core exception helpers.

--- src/Jcd.Utilities/Validations/Argument.cs
+++ src/Jcd.Utilities/Validations/Argument.cs
@@ -11,10 +11,10 @@ namespace Jcd.Utilities.Validations
 
         public static void RaiseExpectationViolation<T>(T expected, T actual, string name=null, string message = null)
         {
-            RaiseArgumentException(message ?? $"Expected {name} to be {expected}, but it was {actual}.", name);
+            RaiseArgumentException(name, message ?? $"Expected {name} to be {expected}, but it was {actual}.");
         }
 
-        public static void RaiseArgumentException(string message, string name=null)
+        public static void RaiseArgumentException(string name = null, string message = null)
         {
             if (Check.IsNull(name) || Check.IsWhitespace(name) || Check.IsEmpty(name)) name = UnspecifiedParamName;
             throw new ArgumentException(message ?? $"{name} contains an invalid value.",name);
@@ -62,25 +62,25 @@ namespace Jcd.Utilities.Validations
         public static void IsEmpty<T>(IEnumerable<T> list, string name = null, string message = null)
         {
             IsNotNull(list, name);
-            Check.IsEmpty(list, onFailure: () => RaiseArgumentException(message ?? $"Expected {name} to be an empty collection, but it contained values.", name));
+            Check.IsEmpty(list, onFailure: () => RaiseArgumentException(name, message ?? $"Expected {name} to be an empty collection, but it contained values."));
         }
 
         public static void HasItems<T>(IEnumerable<T> list, string name = null, string message = null)
         {
             IsNotNull(list, name);
-            Check.HasItems(list, onFailure: () => RaiseArgumentException(message ?? $"Expected {name} to contain at least one item, but it was empty.", name));
+            Check.HasItems(list, onFailure: () => RaiseArgumentException(name, message ?? $"Expected {name} to contain at least one item, but it was empty."));
         }
 
         public static void Contains<T>(IEnumerable<T> list, T target, string name = null, string message = null)
         {
             IsNotNull(list, name);
-            Check.Contains(list, target, onFailure: () => RaiseArgumentException(message ?? $"{target} was not found in {name}.", name));
+            Check.Contains(list, target, onFailure: () => RaiseArgumentException(name, message ?? $"{target} was not found in {name}."));
         }
 
         public static void DoesNotContain<T>(IEnumerable<T> list, T target, string name = null, string message = null)
         {
             IsNotNull(list, name);
-            Check.DoesNotContain(list, target, onFailure: () => RaiseArgumentException(message ?? $"{target} was expected to not be in {name}, but was found.", name));
+            Check.DoesNotContain(list, target, onFailure: () => RaiseArgumentException(name, message ?? $"{target} was expected to not be in {name}, but was found."));
         }
         #endregion
 
@@ -88,12 +88,12 @@ namespace Jcd.Utilities.Validations
         public static void IsEmpty(string value, string name = null, string message = null)
         {
             IsNotNull(value, name);
-            Check.IsEmpty(value, onFailure: () => RaiseArgumentException(message ?? $"Expected {name} to be an empty string, but it contains text.", name));
+            Check.IsEmpty(value, onFailure: () => RaiseArgumentException(name, message ?? $"Expected {name} to be an empty string, but it contains text."));
         }
         public static void HasData(string value, string name = null, string message = null)
         {
             IsNotNull(value, name);
-            Check.HasData(value, onFailure: () => RaiseArgumentException(message ?? $"Expected {name} to be a non-empty string, but it contains text.", name));
+            Check.HasData(value, onFailure: () => RaiseArgumentException(name, message ?? $"Expected {name} to be a non-empty string, but it contains text."));
         }
         public static void IsWhitespace(string value, string name = null, string message = null)
         {
@@ -103,41 +103,41 @@ namespace Jcd.Utilities.Validations
         public static void IsWhitespaceOrEmpty(string value, string name = null, string message = null)
         {
             IsNotNull(value, name);
-            Check.PassesAny(new Check.Signature<string>[] { Check.IsWhitespace, Check.IsEmpty }, value, onFailure: ()=> RaiseArgumentException(message ?? $"Expected {name} to be whitespace or empty.", name));
+            Check.PassesAny(new Check.Signature<string>[] { Check.IsWhitespace, Check.IsEmpty }, value, onFailure: ()=> RaiseArgumentException(name, message ?? $"Expected {name} to be whitespace or empty."));
         }
         public static void IsNullOrWhitespace(string value, string name = null, string message = null)
         {
-            Check.PassesAny(new Check.Signature<string>[] { Check.IsNull, Check.IsWhitespace }, value, onFailure: () => RaiseArgumentException(message ?? $"Expected {name} to be null or whitespace", name));
+            Check.PassesAny(new Check.Signature<string>[] { Check.IsNull, Check.IsWhitespace }, value, onFailure: () => RaiseArgumentException(name, message ?? $"Expected {name} to be null or whitespace"));
         }
         public static void IsNullWhitespaceOrEmpty(string value, string name = null, string message = null)
         {
-            Check.PassesAny(new Check.Signature<string>[] { Check.IsNull, Check.IsWhitespace, Check.IsEmpty }, value, onFailure: () => RaiseArgumentException(message ?? $"Expected {name} to be null, whitespace, or empty", name));
+            Check.PassesAny(new Check.Signature<string>[] { Check.IsNull, Check.IsWhitespace, Check.IsEmpty }, value, onFailure: () => RaiseArgumentException(name, message ?? $"Expected {name} to be null, whitespace, or empty"));
         }
 
         public static void IsNullOrEmpty(string value, string name = null, string message = null)
         {
-            Check.PassesAny(new Check.Signature<string>[] { Check.IsNull, Check.IsEmpty }, value, onFailure: () => RaiseArgumentException(message ?? $"Expected {name} to be null or empty", name));
+            Check.PassesAny(new Check.Signature<string>[] { Check.IsNull, Check.IsEmpty }, value, onFailure: () => RaiseArgumentException(name, message ?? $"Expected {name} to be null or empty"));
         }
 
         public static void IsNotWhitespace(string value, string name = null, string message = null)
         {
-            Check.IsNotWhitespace(value, onFailure: () => RaiseArgumentException(message ?? $"Invalid argument: {name} is all whitespace.", name));
+            Check.IsNotWhitespace(value, onFailure: () => RaiseArgumentException(name, message ?? $"Invalid argument: {name} is all whitespace."));
         }
         public static void IsNotWhitespaceOrEmpty(string value, string name = null, string message = null)
         {
-            Check.FailsAll(new Check.Signature<string>[] { Check.IsWhitespace, Check.IsEmpty }, value, onFailure: () => RaiseArgumentException(message ?? $"Expected {name} to be non-empty and non-whitespace.", name));
+            Check.FailsAll(new Check.Signature<string>[] { Check.IsWhitespace, Check.IsEmpty }, value, onFailure: () => RaiseArgumentException(name, message ?? $"Expected {name} to be non-empty and non-whitespace."));
         }
         public static void IsNotNullOrWhitespace(string value, string name = null, string message = null)
         {
-            Check.FailsAll(new Check.Signature<string>[] { Check.IsNull, Check.IsWhitespace }, value, onFailure: () => RaiseArgumentException(message ?? $"Expected {name} to be null, empty, or whitespace.", name));
+            Check.FailsAll(new Check.Signature<string>[] { Check.IsNull, Check.IsWhitespace }, value, onFailure: () => RaiseArgumentException(name, message ?? $"Expected {name} to be null, empty, or whitespace."));
         }
         public static void IsNotNullWhitespaceOrEmpty(string value, string name = null, string message = null)
         {
-            Check.FailsAll(new Check.Signature<string>[] { Check.IsNull, Check.IsEmpty, Check.IsWhitespace }, value, onFailure: () => RaiseArgumentException(message ?? $"Expected {name} to be non-null, non-empty, and non-whitespace.", name));
+            Check.FailsAll(new Check.Signature<string>[] { Check.IsNull, Check.IsEmpty, Check.IsWhitespace }, value, onFailure: () => RaiseArgumentException(name, message ?? $"Expected {name} to be non-null, non-empty, and non-whitespace."));
         }
         public static void IsNotNullOrEmpty(string value, string name = null, string message = null)
         {
-            Check.FailsAll(new Check.Signature<string>[] { Check.IsNull, Check.IsEmpty }, value, onFailure: () => RaiseArgumentException(message ?? $"Expected {name} to be non-null and non-empty.", name));
+            Check.FailsAll(new Check.Signature<string>[] { Check.IsNull, Check.IsEmpty }, value, onFailure: () => RaiseArgumentException(name, message ?? $"Expected {name} to be non-null and non-empty."));
         }
         #endregion
 
@@ -145,37 +145,37 @@ namespace Jcd.Utilities.Validations
 
         public static void AreSameObject(object value, object comparison, string name = null, string message = null)
         {
-            Check.AreSameObject(value, comparison, onFailure: () => RaiseArgumentException(message ?? $"Object for {name} was not the expected instance.", name));
+            Check.AreSameObject(value, comparison, onFailure: () => RaiseArgumentException(name, message ?? $"Object for {name} was not the expected instance."));
         }
 
         public static void IsGreaterThan<T>(T value, T comparison, string name = null, string message = null)
             where T : IComparable<T>
         {
-            Check.IsGreaterThan(value, comparison,onFailure:()=>RaiseArgumentException(message ?? $"Value for {name} ({value}) was expected to be greater than {comparison}",name));
+            Check.IsGreaterThan(value, comparison,onFailure:()=>RaiseArgumentException(name, message ?? $"Value for {name} ({value}) was expected to be greater than {comparison}"));
         }
 
         public static void IsGreaterThanOrEqual<T>(T value, T comparison, string name = null, string message = null)
             where T : IComparable<T>
         {
-            Check.Passes(() => value.CompareTo(comparison) >= 0, onFailure: ()=>RaiseArgumentException(message ?? $"Value for {name} ({value}) was expected to be greater than or equal to {comparison}",name));
+            Check.Passes(() => value.CompareTo(comparison) >= 0, onFailure: ()=>RaiseArgumentException(name, message ?? $"Value for {name} ({value}) was expected to be greater than or equal to {comparison}"));
         }
 
         public static void IsLessThan<T>(T value, T comparison, string name = null, string message = null)
             where T : IComparable<T>
         {
-            Check.IsLessThan(value, comparison, onFailure: () => RaiseArgumentException(message ?? $"Value for {name} ({value}) was expected to be less than {comparison}", name));
+            Check.IsLessThan(value, comparison, onFailure: () => RaiseArgumentException(name, message ?? $"Value for {name} ({value}) was expected to be less than {comparison}"));
         }
 
         public static void IsLessThanOrEqual<T>(T value, T comparison, string name = null, string message = null)
             where T : IComparable<T>
         {
-            Check.Passes(() => value.CompareTo(comparison) >= 0, onFailure: () => RaiseArgumentException(message ?? $"Value for {name} ({value}) was expected to be less than {comparison}", name));
+            Check.Passes(() => value.CompareTo(comparison) >= 0, onFailure: () => RaiseArgumentException(name, message ?? $"Value for {name} ({value}) was expected to be less than {comparison}"));
         }
 
         public static void AreEqual<T>(T value, T comparison, string name = null, string message = null)
             where T : IComparable<T>
         {
-            Check.AreEqual(value, comparison, onFailure: () => RaiseArgumentException(message ?? $"Value for {name} ({value}) is not equal to {comparison}", name));
+            Check.AreEqual(value, comparison, onFailure: () => RaiseArgumentException(name, message ?? $"Value for {name} ({value}) is not equal to {comparison}"));
         }
 
         public static void InRange<T>(T value, T min, T max, string name = null, string message = null)
@@ -195,32 +195,32 @@ namespace Jcd.Utilities.Validations
         #region custom and multi-condition operations
         public static void Passes<T>(Check.Signature<T> condition, T value, string name = null, string message = null)
         {
-            Check.Passes(condition, value, onFailure:() => RaiseArgumentException(message,name));
+            Check.Passes(condition, value, onFailure:() => RaiseArgumentException(name, message));
         }
 
         public static void Fails<T>(Check.Signature<T> condition, T value, string name = null, string message = null)
         {
-            Check.Passes(condition, value, onSuccess: () => RaiseArgumentException(message, name));
+            Check.Passes(condition, value, onSuccess: () => RaiseArgumentException(name, message));
         }
 
         public static void PassesAll<T>(IEnumerable<Check.Signature<T>> conditions, T value, string name = null, string message = null)
         {
-            Check.PassesAll(conditions, value, onFailure: () => RaiseArgumentException(message, name));
+            Check.PassesAll(conditions, value, onFailure: () => RaiseArgumentException(name, message));
         }
 
         public static void PassesAny<T>(IEnumerable<Check.Signature<T>> conditions, T value, string name = null, string message = null)
         {
-            Check.PassesAny(conditions, value, onFailure: () => RaiseArgumentException(message, name));
+            Check.PassesAny(conditions, value, onFailure: () => RaiseArgumentException(name, message));
         }
 
         public static void FailsAll<T>(IEnumerable<Check.Signature<T>> conditions, T value, string name = null, string message = null)
         {
-            Check.FailsAll(conditions, value, onFailure: () => RaiseArgumentException(message, name));
+            Check.FailsAll(conditions, value, onFailure: () => RaiseArgumentException(name, message));
         }
 
         public static void FailsAny<T>(IEnumerable<Check.Signature<T>> conditions, T value, string name = null, string message = null)
         {
-            Check.FailsAny(conditions, value, onFailure: () => RaiseArgumentException(message, name));
+            Check.FailsAny(conditions, value, onFailure: () => RaiseArgumentException(name, message));
         }
         #endregion
     }
--- test/Jcd.Utilities.Test/Validations/ArgumentTests.cs
+++ test/Jcd.Utilities.Test/Validations/ArgumentTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Jcd.Utilities.Validations;
+using System;
 using System.Linq;
 using Xunit;
 
@@ -6,9 +7,340 @@ namespace Jcd.Utilities.Test.Validations
 {
     public class ArgumentTests
     {
-        [Fact]
-        public void TestMethod1()
+        #region exception helpers		
+
+        [Theory]
+        [InlineData(0,1,null,null)]
+        [InlineData(0, 1, null, "message")]
+        [InlineData(0, 1, "param", "message")]
+        [InlineData(0, 1, "param", null)]
+        [InlineData(0, 1, "", null)]
+        [InlineData(0, 1, "", "message")]
+        [InlineData(0, 1, " ", null)]
+        [InlineData(0, 1, " ", "message")]
+        public void RaiseExpectationViolation(int expected, int actual, string paramName, string message)
         {
+            var ex=Assert.Throws<ArgumentException>(() => Argument.RaiseExpectationViolation<int>(expected, actual, paramName, message));
+            if (message == null)
+            {
+                Assert.Contains("Expect", ex.Message);
+                Assert.Contains("to be", ex.Message);
+                Assert.Contains("it was", ex.Message);
+            }
+            else
+            {
+                Assert.StartsWith(message,ex.Message);
+            }
+            if (string.IsNullOrWhiteSpace(paramName))
+            {
+                Assert.StartsWith(Argument.UnspecifiedParamName, ex.ParamName );
+            }
+            else
+            {
+                Assert.StartsWith(paramName, ex.ParamName);
+            }
         }
+
+        [Theory]
+        [InlineData("param",null)]
+        [InlineData("param","message")]
+        [InlineData(null, "message")]
+        [InlineData("", "message")]
+        [InlineData(" ", "message")]
+        public void RaiseArgumentException(string paramName, string message)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => Argument.RaiseArgumentException(paramName, message));
+            if (message == null)
+            {
+                Assert.Contains("contains an invalid value", ex.Message);
+            }
+            else
+            {
+                Assert.StartsWith(message, ex.Message);
+            }
+            if (string.IsNullOrWhiteSpace(paramName))
+            {
+                Assert.StartsWith(Argument.UnspecifiedParamName, ex.ParamName);
+            }
+            else
+            {
+                Assert.StartsWith(paramName, ex.ParamName);
+            }
+        }
+
+        [Theory]
+        [InlineData("param", null)]
+        [InlineData("param", "message")]
+        [InlineData(null, "message")]
+        [InlineData("", "message")]
+        [InlineData(" ", "message")]
+        public void RaiseArgumentNullException(string paramName, string message)
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => Argument.RaiseArgumentNullException(paramName, message));
+            if (message == null)
+            {
+                Assert.Contains("expected non-null", ex.Message);
+            }
+            else
+            {
+                Assert.StartsWith(message, ex.Message);
+            }
+            if (string.IsNullOrWhiteSpace(paramName))
+            {
+                Assert.StartsWith(Argument.UnspecifiedParamName, ex.ParamName);
+            }
+            else
+            {
+                Assert.StartsWith(paramName, ex.ParamName);
+            }
+        }
+
+        [Theory]
+        [InlineData(1, 2, 5, "param", null)]
+        [InlineData(1, 2, 5, "param", "message")]
+        [InlineData(1, 2, 5, null, "message")]
+        [InlineData(1, 2, 5, "", "message")]
+        [InlineData(1, 2, 5, " ", "message")]
+        public void RaiseArgumentOutOfRangeException_WithNullMessage_ExpectExceptionWithDefaultMessage(int actual, int min, int max, string paramName, string message)
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => Argument.RaiseArgumentOutOfRangeException<int>(actual,min,max,paramName, message));
+            if (message == null)
+            {
+                Assert.Contains("Expected value within range", ex.Message);
+                Assert.Contains(actual.ToString(), ex.Message);
+                Assert.Contains(min.ToString(), ex.Message);
+                Assert.Contains(max.ToString(), ex.Message);
+            }
+            else
+            {
+                Assert.StartsWith(message, ex.Message);
+            }
+            if (string.IsNullOrWhiteSpace(paramName))
+            {
+                Assert.StartsWith(Argument.UnspecifiedParamName, ex.ParamName);
+            }
+            else
+            {
+                Assert.StartsWith(paramName, ex.ParamName);
+            }
+        }
+
+        #endregion
+
+        #region Boolean and Null checks
+        [Fact]
+        public void IsTrue()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void IsFalse()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void IsNotNull()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void IsNull()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region collection operations
+        [Fact]
+        public void IsEmptyCollection()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void HasItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Contains()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void DoesNotContain()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region string operations
+        [Fact]
+        public void IsEmptyString()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void HasData()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void IsWhitespace()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void IsWhitespaceOrEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void IsNullOrWhitespace()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void IsNullWhitespaceOrEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void IsNullOrEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void IsNotWhitespace()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void IsNotWhitespaceOrEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void IsNotNullOrWhitespace()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void IsNotNullWhitespaceOrEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void IsNotNullOrEmpty()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region range and relational operations
+
+        [Fact]
+        public void AreSameObject()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void IsGreaterThan()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void IsGreaterThanOrEqual()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void IsLessThan()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void IsLessThanOrEqual()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void AreEqual()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void InRange()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void NotInRange()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region custom and multi-condition operations
+        [Fact]
+        public void Passes()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Fails()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void PassesAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void PassesAny()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void FailsAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void FailsAny()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
     }
 }
